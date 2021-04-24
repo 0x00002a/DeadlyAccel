c91be704692316dd6311557d02361caa5cf78a16
Natasha England-Elbro <ashenglandelbro@protonmail.com>
2021-04-24 15:17:33 +0100

    fix: Client DC on using medbay

--- Data/Scripts/DeadlyAccel/PlayerManager.cs
+++ Data/Scripts/DeadlyAccel/PlayerManager.cs
@@ -235,8 +235,11 @@ namespace Natomic.DeadlyAccel
 
         public void ApplyToxicityDecay(long pid, float multiplier)
         {
-            var data = players_lookup_[pid];
-            data.toxicity_buildup = Math.Max(data.toxicity_buildup - data.lowest_toxic_decay * multiplier, 0);
+            if (players_lookup_.ContainsKey(pid))
+            {
+                var data = players_lookup_[pid];
+                data.toxicity_buildup = Math.Max(data.toxicity_buildup - data.lowest_toxic_decay * multiplier, 0);
+            }
         }
 
        private void ApplyToxicityDecay(PlayerData data)
