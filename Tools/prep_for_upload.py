78b33f9c59d4a7fe3cb67f5c6ea4dec55a4e34c2
Natasha England-Elbro <ashenglandelbro@protonmail.com>
2022-03-10 13:21:47 +0000

    chore: misc fixes

--- Tools/prep_for_upload.py
+++ Tools/prep_for_upload.py
@@ -1,7 +1,7 @@
 
 import sys 
 import os
-
+import shutil
 
 prep_roots = ["Data", "Textures", "Models", "Audio", "LICENSE", "README.md", "thumb.png"]
 
@@ -17,28 +17,27 @@ def copy_path(path: str, readin: [str]):
         readin.append(path)
 
 
-def copy_to(prefix: str, files: [str]):
+def copy_to(prefix: str, rmprefix: str, files: [str]):
     for file in files:
-        dst = os.path.join(prefix, file)
+        dst = os.path.join(prefix, file[len(rmprefix) + 1:] if file.startswith(rmprefix) else file)
         print("{} -> {}".format(file, dst))
         parent_dir = os.path.dirname(dst)
         if parent_dir and not os.path.exists(parent_dir):
+            print("makedir: {}".format(parent_dir))
             os.makedirs(parent_dir)
-        if not os.path.exists(dst):
-            os.link(file, dst)  
-            
-        else:
-            print("Skipping '{}' because it already exists".format(dst))
+        
+        shutil.copyfile(file, dst)
 
 if __name__ == '__main__':
     infile = sys.argv[1]
     outfile = sys.argv[2]
+    outfile = outfile.replace('"', '')
     
     to_copy = []
     for file in os.listdir(infile):
         if (os.path.basename(file) + os.path.splitext(file)[1]) in prep_roots:
-            copy_path(file, to_copy)
-    copy_to(outfile, to_copy)
+            copy_path(os.path.join(infile, file), to_copy)
+    copy_to(outfile, infile, to_copy)
     
     
     
